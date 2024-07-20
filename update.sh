3b866c9af3fbfa0bcf1eb70e60ac74199198d0b9
AkechiShiro <14914796+AkechiShiro@users.noreply.github.com>
2024-07-20 14:35:03 +0200

    Fix typo in updpkgsums

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 set -x
 pkgver_new=$(curl https://portswigger.net/burp/releases | grep community | sed -nE "s/.*professional-community-(.*)\" class=\".*/\1/p" | head -n  1 | tr -d '\n' | tr '-' '.')
 sed -Ei "1,\$s|^(pkgver=).*|\1$pkgver_new|" PKGBUILD
-updkgsums ./PKGBUILD
+updpkgsums ./PKGBUILD
 makepkg --printsrcinfo > .SRCINFO
 makepkg PKGBUILD --clean --cleanbuild --force
 

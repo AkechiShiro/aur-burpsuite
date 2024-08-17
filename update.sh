77619834dff9d2260755a1555def5cd44826a727
AkechiShiro <14914796+AkechiShiro@users.noreply.github.com>
2024-08-17 21:32:38 +0200

    Fix CI update of latest_version

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -x
-pkgver_new=$(curl https://portswigger.net/burp/releases | grep community | sed -nE "s/.*professional-community-(.*)\" class=\".*/\1/p" | head -n  1 | tr -d '\n' | tr '-' '.')
+export pkgver_new=$(curl https://portswigger.net/burp/releases | grep community | sed -nE "s/.*professional-community-(.*)\" class=\".*/\1/p" | head -n  1 | tr -d '\n' | tr '-' '.')
+echo "$pkgver_new" > latest_version
 sed -Ei "1,\$s|^(pkgver=).*|\1$pkgver_new|" PKGBUILD
 updpkgsums ./PKGBUILD
 makepkg --printsrcinfo > .SRCINFO

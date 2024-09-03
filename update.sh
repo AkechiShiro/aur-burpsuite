3f75c561b08182af18914aa9cc56348d21a880a8
AkechiShiro <14914796+AkechiShiro@users.noreply.github.com>
2024-09-03 18:18:22 +0200

    Use PortSwigger CDN

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -x
+# curl must use portswigger as cdn won't show all the versions available.
 export pkgver_new=$(curl https://portswigger.net/burp/releases | grep community | sed -nE "s/.*professional-community-(.*)\" class=\".*/\1/p" | head -n  1 | tr -d '\n' | tr '-' '.')
 echo "$pkgver_new" > latest_version
 sed -Ei "1,\$s|^(pkgver=).*|\1$pkgver_new|" PKGBUILD
